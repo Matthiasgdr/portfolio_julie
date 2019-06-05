163baa0020b5df21e32e5aa4500e64fc7d058dc4
Wizeeps <spam68130@gmail.com>
2019-06-05 18:28:58 +0200

    Add FullpageHs

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@ import 'reset-css';
 import './css/style.styl';
 import './js/script.js';
 
+// Optional. When using fullPage extensions
+//import scrollHorizontally from './fullpage.scrollHorizontally.min';
+
+// Optional. When using scrollOverflow:true
+//import IScroll from 'fullpage.js/vendors/scrolloverflow';
+
+// Importing fullpage.js
+import fullpage from 'fullpage.js';
+
+// When using fullPage extensions replace the previous import
+// of fullpage.js for this file
+//import fullpage from 'fullpage.js/dist/fullpage.extensions.min';
+
+// Initializing it
+var fullPageInstance = new fullpage('.fullpage', {
+    navigation: true,
+    sectionsColor:['transparent', 'transparent', 'transparent', 'transparent']
+});
